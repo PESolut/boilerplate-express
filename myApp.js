5b1cb961b11d392309bdc04944f63c56ccf39f60
PESolut <jahaadpetty@pursuit.org>
2023-07-23 15:29:27 -0400

    created logger middleware to log out requests in console

--- myApp.js
+++ myApp.js
@@ -5,7 +5,17 @@ require('dotenv').config()
 let indexAbsolutePath = __dirname + '/views/index.html'
 let assetsAbsolutePath = __dirname + '/public'
 
+// allow access to our public assets
 app.use("/public", express.static(assetsAbsolutePath));
+
+// logger middleware function
+app.use((req,res,next )=> {
+    let requestMethod = req.method
+    let requestPath = req.path
+    let requestIP = req.ip
+    console.log(`${requestMethod} ${requestPath} - ${requestIP}`)
+    next()
+})
 
 console.log("Hello World")
 

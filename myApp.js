e82e1355b8a44cb1664d4cb92f337c7185caa3d7
PESolut <jahaadpetty@pursuit.org>
2023-07-23 14:36:42 -0400

    using assets at /public

--- myApp.js
+++ myApp.js
@@ -3,6 +3,10 @@ let app = express();
 let indexAbsolutePath = __dirname + '/views/index.html'
 let assetsAbsolutePath = __dirname + '/public'
 
+// app.use(express.static(assetsAbsolutePath))
+app.use("/public", express.static(assetsAbsolutePath));
+
+
 console.log("Hello World")
 
 app.get('/',(req, res)=> {
@@ -10,9 +14,6 @@ app.get('/',(req, res)=> {
     res.sendFile(indexAbsolutePath)
 
 })
-
-app.use(express.static(assetsAbsolutePath))
-
 
 
 
